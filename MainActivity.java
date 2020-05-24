ac1cefabc16205cbdaa6a3b8a5c1389696730790
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-05-24 16:25:22 +0530

    simple animation app

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,37 @@
-package com.example.videoapp;
+package com.example.animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean bartIsShowing = true;
+    public void fade(View view){
+        Log.i("Info","image tapped!");
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+        ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+        bartImageView.animate().alpha(0).setDuration(2000);
+        homerImageView.animate().alpha(1).setDuration(2000);
+
+        if (bartIsShowing){
+            bartIsShowing = false;
+            bartImageView.animate().alpha(0).setDuration(2000);
+            homerImageView.animate().alpha(1).setDuration(2000);
+
+        }else{
+            bartIsShowing = true;
+            bartImageView.animate().alpha(1).setDuration(2000);
+            homerImageView.animate().alpha(0).setDuration(2000);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        VideoView videoView =  (VideoView) findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.covid );
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
-        videoView.start();
-
     }
 }
