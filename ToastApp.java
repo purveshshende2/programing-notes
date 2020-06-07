00886f3c3305a4d30977253e36f9eec730b8a715
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-06-07 16:16:28 +0530

    Create Simple-toast-app

---
+++ ToastApp.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void nameButton(View view){
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Log.i("Info","Button is running!");
+        Toast.makeText(this, "hi" + " "+ editText.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
